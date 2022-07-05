7c99b9193f42a45fd28515bfd683236bce117a38
Michael Friend <michael.l.friend2@gmail.com>
2022-07-05 15:56:50 -0500

    updated

---
+++ src/main/java/Student.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Student {
+    private long id;
+    private String name;
+    private List<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+}
